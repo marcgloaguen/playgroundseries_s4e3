ff9634e143f3b75260e52c816529b2a51d1ab301
Marc GLOAGUEN <marc.gloaguen@gmail.com>
2024-03-02 18:17:10 +0100

    modified:   eda.ipynb 	deleted:    main.py 	new file:   module.py

---
+++ module.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def load_data(name: str) -> pd.DataFrame:
+    return pd.read_csv(f"data/{name}.csv", index_col='id')
+
+
+def feature_engineering(data: pd.DataFrame) -> pd.DataFrame:
+    return data
+
+
+def split_data(data: pd.DataFrame, targets_name: list, random_state: int):
+    features = data.drop(columns=targets_name)
+    targets = data[targets_name]
+    return train_test_split(features, targets, 0.2, random_state=random_state)
