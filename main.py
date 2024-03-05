d35864b3fc501cdd453fb9e1fb47de394dfa9334
Marc GLOAGUEN <marc.gloaguen@gmail.com>
2024-03-05 19:51:41 +0100

    modified:   eda.ipynb 	modified:   main.py

--- main.py
+++ main.py
@@ -1,21 +1,53 @@
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+from xgboost import XGBClassifier
 
 def load_data(name: str) -> pd.DataFrame:
-    return pd.read_csv(f"data/{name}.csv", index_col='id')
+    print(f"Load Data : {name}")
+    return pd.read_csv(f"data/{name}", index_col='id')
 
 
 def feature_engineering(data: pd.DataFrame) -> pd.DataFrame:
     return data
 
 
-def split_data(data: pd.DataFrame, targets_name: list, random_state: int):
+def val_score(model, features, targets, metrics=None) -> None:
+    scores = cross_val_score(
+        model,
+        X=features,
+        y=targets,
+        scoring=metrics,
+        cv=4,
+        n_jobs=-1,
+        verbose=20
+        )
+    print(f'mean {metrics} : {np.mean(scores):.5f} ± {np.std(scores):.5f}')
+
+
+def split_features_targets(data: pd.DataFrame, targets_name: list)-> tuple:
     features = data.drop(columns=targets_name)
     targets = data[targets_name]
-    return train_test_split(features, targets, 0.2, random_state=random_state)
+    return features, targets
 
 
 if __name__ == "__main__":
-    train = load_data('train')
-    test = load_data('test')
+    TARGETS = [
+        'Pastry',
+        'Z_Scratch',
+        'K_Scatch',
+        'Stains',
+        'Dirtiness',
+        'Bumps',
+        'Other_Faults'
+        ]
+    SEED = 42
+    SPLITS = 5
+    train = load_data('train.csv')
+    test = load_data('test.csv')
+    submission = load_data('sample_submission.csv')
+    model = XGBClassifier()
+    X, Y = split_features_targets(train, TARGETS)
+    
+    val_score(model, X, Y, 'roc_auc')
